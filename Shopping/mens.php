602ed17beebf9f864623316bf5bc454b83efdcb6
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2024-01-14 23:26:04 +0800

    mens.php
    removed the shopping cart

--- Shopping/mens.php
+++ Shopping/mens.php
@@ -29,13 +29,6 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                 </a></li>
-
-            <li class="icon-2"><a href="#"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1" />
-                    </svg>
-                </a></li>
-        </ul>
-
     </div>
 
     <!-- Content -->
